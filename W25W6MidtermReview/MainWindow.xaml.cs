bd4bb0fb5e560add7c49a1dab895251b8366c044
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-02-14 16:58:11 -0500

    calculate btn

--- W25W6MidtermReview/MainWindow.xaml.cs
+++ W25W6MidtermReview/MainWindow.xaml.cs
@@ -16,8 +16,54 @@ namespace W25W6MidtermReview;
 /// </summary>
 public partial class MainWindow : Window
 {
+    Employee _emp;
+    List<Employee> _employees = new List<Employee>();
+
     public MainWindow()
     {
         InitializeComponent();
+        rdoHourly.IsChecked = true;
+    }
+
+    private void rdoCommission_Checked(object sender, RoutedEventArgs e)
+    {
+        lblInput2.Content = "Gross Sales";
+        lblInput3.Content = "Commission Rate";
+    }
+
+    private void rdoHourly_Checked(object sender, RoutedEventArgs e)
+    {
+        lblInput2.Content = "Hours Worked";
+        lblInput3.Content = "Hourly Wage";
+    }
+
+    private void btnCalculate_Click(object sender, RoutedEventArgs e)
+    {
+        string name = txtName.Text;
+
+        if (rdoHourly.IsChecked == true)
+        {
+            int hours = int.Parse(txtInput2.Text);
+            double wage = double.Parse(txtInput3.Text);
+
+            _emp = new HourlyEmployee(name, hours, wage);
+        }
+        else
+        {
+            double grossSales = double.Parse(txtInput2.Text);
+            double commissionRate = double.Parse(txtInput3.Text) / 100;
+
+            _emp = new CommissionEmployee(name, grossSales, commissionRate);
+        }
+
+        txtGrossEarnings.Text = _emp.GrossEarnings().ToString("C");
+        txtTax.Text = _emp.Tax().ToString("C");
+        txtNetEarnings.Text = _emp.NetEarnings().ToString("C");
+
+        // add name to the listbox
+        lstEmployees.Items.Add(name);
+
+        // add the emp object to the List<>
+        _employees.Add(_emp);
     }
 }
