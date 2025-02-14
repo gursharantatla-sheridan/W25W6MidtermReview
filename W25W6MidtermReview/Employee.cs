95bd0d4f3b90596385ac92de25ed95923e63f156
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-02-14 16:18:12 -0500

    emp class

---
+++ W25W6MidtermReview/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W25W6MidtermReview
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+
+        public Employee(string name)
+        {
+            Name = name;
+        }
+
+        public abstract double GrossEarnings();
+
+        public double Tax()
+        {
+            return GrossEarnings() * 0.20;
+        }
+
+        public double NetEarnings()
+        {
+            return GrossEarnings() - Tax();
+        }
+    }
+}
